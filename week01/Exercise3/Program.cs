3f8305953e6d960e210466ba8e8ee7ccaa5fc84b
Arddun27 <srichins@byupathway.edu>
2026-01-09 14:52:25 -0700

    Finished exersize 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int userGuess = -1;
+        string userInput = "";
+
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine("What is your guess");
+            userInput = Console.ReadLine();
+            userGuess = int.Parse(userInput);
+
+            if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
