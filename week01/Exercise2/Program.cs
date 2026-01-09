165b40b57afa45adf74480bed862785d978c1101
Arddun27 <srichins@byupathway.edu>
2026-01-09 13:48:25 -0700

    Finished exersize 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string gradeInput = Console.ReadLine();
+        int gradePercent = int.Parse(gradeInput);
+        string letterGrade = "";
+        bool passing = true;
+        string passFail = "";
+        
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+            passing = true;
+        }
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B";
+            passing = true;
+        }
+        else if (gradePercent >= 70)
+        {
+            letterGrade = "C";
+            passing = true;
+        }
+        else if (gradePercent >= 60)
+        {
+            letterGrade = "D";
+            passing = false;
+        }
+        else if (gradePercent < 60)
+        {
+            letterGrade = "F";
+            passing = false;
+        }  
+
+        if (passing == true)
+        {
+            passFail = "passed!";
+        }
+        else
+        {
+            passFail = "failed.";
+        }
+
+        Console.WriteLine($"Your grade is a {letterGrade} you {passFail}");
     }
+
+        
 }
